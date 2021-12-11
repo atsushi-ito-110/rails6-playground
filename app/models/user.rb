25fd2fd0cc12fa1d7b8fd98798adf1971bfc1d9e
Atsushi Ito <atsushi.itoh66@gmail.com>
2021-12-11 17:18:03 +0900

    feat: devise: Trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        # TODO 一旦作ったらここらへんやる :confirmable, :omniauthable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable
+        # TODO 一旦作ったらここらへんやる , :confirmable, :omniauthable
 end
