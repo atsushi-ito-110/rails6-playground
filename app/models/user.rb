18862cb3ac923bea55d28797cc8da6dca73cb651
Atsushi Ito <atsushi.itoh66@gmail.com>
2021-12-11 23:13:02 +0900

    feat: google oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,27 @@ class User < ApplicationRecord
     validates :name
     validates :email
   end
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
 
-    # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-      logger.info(data)
-      user = User.create!(
-        email: data['email'],
-        password: Devise.friendly_token[0,20]
-      )
+  class << self
+    def from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(email: data['email']).first
+
+      unless user
+        logger.info(data)
+        user = User.create!(
+          email: data['email'],
+          name: email_to_name_by_before_at_mark(data['email']),
+          password: Devise.friendly_token[0,20],
+          confirmed_at: Time.now
+        )
+      end
+      user
     end
-    user
+
+    private
+    def email_to_name_by_before_at_mark(email)
+      /^[a-zA-Z0-9_.+-]+/.match(email).to_s
+    end
   end
 end
